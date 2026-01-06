0974e06dabadbbd2b8c50881e25e1582d015f15d
Katon Surya Atmaja <katonatmaja@gmail.com>
2026-01-06 16:58:51 +0700

    Add more shadcn/ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import { ThemeProvider } from "@/components/theme-provider"
 import { Button } from "@/components/ui/button"
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Card,
+  CardContent,
+} from "@/components/ui/card"
 import { ModeToggle } from './components/mode-toggle'
 
 function App() {
@@ -12,28 +13,17 @@ function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <>
-        <div className="flex flex-col items-center">
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <Button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </Button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-        <div className="mt-5">
-          <ModeToggle/>
+        <div className="container mx-auto px-4 py-8 space-y-4">
+          <Card>
+            <CardContent>
+              <Button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+              </Button>
+            </CardContent>
+          </Card>
+          <div>
+            <ModeToggle />
+          </div>
         </div>
       </>
     </ThemeProvider>
